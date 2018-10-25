f7dbc3f1cf84eaa216f598b19a5c0187a3942131
Bartosz Borawski <bartosz.borawski@softax.pl>
2018-10-25 12:48:47 +0200

    little fixes

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json({ limit: '50mb' }));
 app.use(bodyParser.urlencoded({ limit: '50mb', extended: false }));
 
-// mongoose.connect('mongodb://username:password@ds115573.mlab.com:15573/paints');
+// mongoose.connect('mongodb://12345678:1234qwer@ds115573.mlab.com:15573/paints');
 
 app.use('/', router);
 
